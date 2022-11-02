92baec93472174862906037feb025c44479ca33f
jaemoon <jaemoon406@gmail.com>
2022-11-02 23:58:30 +0900

    [Add] asgi Router websocket 추가

--- django_channels_prac/asgi.py
+++ django_channels_prac/asgi.py
@@ -1,15 +1,22 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels_prac.settings')
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels_prac.settings")
+
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-})
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
