b3d043f5ee65955c2abb3551f6b83d57b0a0b062
jaemoon <jaemoon406@gmail.com>
2022-11-03 00:02:01 +0900

    [Create] index, room view 생성

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "chat/template/chat/index.html")
+
+def room(request, room_name):
+    return render(request, "chat/template/chat/room.html", {"room_name": room_name})
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request):
+        print(request)
+        return JsonResponse({1: 1})
+    # return render(request, "chat/template/chat/index.html")
